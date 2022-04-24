9e3c7572feb81f98a3336c94e8c6002f978afad8
Suhye5n <sy1599@gmail.com>
2022-04-25 00:17:35 +0900

    footer & top button

--- js/main.js
+++ js/main.js
@@ -17,21 +17,38 @@ searchInputEl.addEventListener('blur', function () {
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
   if (window.scrollY > 500) {
+    //배지 숨기기
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
     });
+    // 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
   } else {
+    //배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    //버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -125,3 +142,7 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl,'show')
     .addTo(new ScrollMagic.Controller());
 });
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
